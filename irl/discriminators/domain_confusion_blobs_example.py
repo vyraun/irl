9f2ee386328eb23143ba8656e63b69470c184907
Bradly Stadie <bstadie@berkeley.edu>
2016-07-09 14:48:25 -0700

    nc

--- irl/discriminators/domain_confusion_blobs_example.py
+++ irl/discriminators/domain_confusion_blobs_example.py
@@ -11,8 +11,8 @@ from utils import *
 Xs, ys = make_blobs(300, centers=[[0, 0], [0, 1]], cluster_std=0.2)
 Xt, yt = make_blobs(300, centers=[[1, -1], [1, 0]], cluster_std=0.2)
 
-plt.scatter(Xs[:,0], Xs[:,1], c=ys, alpha=0.4)
-plt.scatter(Xt[:,0], Xt[:,1], c=yt, cmap='cool', alpha=0.4)
+plt.scatter(Xs[:, 0], Xs[:, 1], c=ys, alpha=0.4)
+plt.scatter(Xt[:, 0], Xt[:, 1], c=yt, cmap='cool', alpha=0.4)
 plt.show()
 #import time
 #time.sleep(10)
